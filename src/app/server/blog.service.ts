b819ad1a5c5f43e75280101ee1e310344e140317
daibede <xuandai300092@gmail.com>
2022-06-10 15:28:12 +0700

    sever blog and category

--- src/app/server/blog.service.ts
+++ src/app/server/blog.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Iblog } from '../model/blog';
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
+  URL_API = `http://localhost:3000/blogs`
+  constructor(
+    private http: HttpClient
+  ) { }
+  listBlog() : Observable<Iblog[]>{
+    return this.http.get<Iblog[]>(`${this.URL_API}`);
+  }
+  detailBlog(id: any) : Observable<Iblog[]>{
+    return this.http.get<Iblog[]>(`${this.URL_API}/${id}`);
+  }
+  deleteBlog(id: any) : Observable<Iblog[]>{
+    return this.http.delete<Iblog[]>(`${this.URL_API}/${id}`);
+  }
+  addBlog(blog: Iblog) : Observable<Iblog[]>{
+    return this.http.post<Iblog[]>(`${this.URL_API}`, blog);
+  }
+  updateBlog(blog: Iblog) : Observable<Iblog[]>{
+    return this.http.put<Iblog[]>(`${this.URL_API}/${blog.id}`, blog);
+  }
 
-  constructor() { }
 }
---
+++ src/app/server/category.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Icategory } from '../model/category';
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  URL_API = `http://localhost:3000/categorys`
+  constructor(
+    private http: HttpClient
+  ) { }
+  
+  listCate(): Observable<Icategory[]>{
+    return this.http.get<Icategory[]>(`${this.URL_API}`)
+  }
+
+  detailCate(id: any): Observable<Icategory>{
+    return this.http.get<Icategory>(`${this.URL_API}/${id}`)
+  }
+
+  deleteCate(id: any): Observable<Icategory>{
+    return this.http.delete<Icategory>(`${this.URL_API}/${id}`)
+  }
+
+  addCate(category: Icategory): Observable<Icategory>{
+    return this.http.post<Icategory>(`${this.URL_API}`, category)
+  }
+
+  updateCate(category: Icategory): Observable<Icategory>{
+    return this.http.put<Icategory>(`${this.URL_API}/${category.id}`, category)
+  }
+  
+  
+  
+  
+}

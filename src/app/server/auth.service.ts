282ca18c36e3479c90196ad5c95e6b43cb7b72a4
daibede <xuandai300092@gmail.com>
2022-06-10 15:50:56 +0700

    server auth

--- src/app/server/auth.service.ts
+++ src/app/server/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Iauth } from '../model/auth';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  URL_API = `http://localhost:3000/users`
+  constructor(
+    private http: HttpClient
+  ) { }
+    listUser(): Observable<Iauth[]>{
+      return this.http.get<Iauth[]>(`${this.URL_API}`)
+    }
+    addUser(user: Iauth): Observable<Iauth>{
+      return this.http.post<Iauth>(`${this.URL_API}`, user)
+    }
+    updateUser(user: Iauth): Observable<Iauth>{
+      return this.http.put<Iauth>(`${this.URL_API}/${user.id}`, user)
+    }
+    deleteUser(id: any): Observable<Iauth>{
+      return this.http.delete<Iauth>(`${this.URL_API}/${id}`)
+    }
 
-  constructor() { }
+
+    signin(user : Iauth): Observable<Iauth> {
+      return this.http.post<Iauth>(`http://localhost:3000/signin`, user)
+    }
 }

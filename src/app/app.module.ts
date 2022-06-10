5ba3a1b1b362567c6462f00e0547de818acdf532
daibede <xuandai300092@gmail.com>
2022-06-10 14:52:34 +0700

    thu muc components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,43 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { WorkComponent } from './pages/work/work.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { ListBlogComponent } from './components/blog/list-blog/list-blog.component';
+import { AddBlogComponent } from './components/blog/add-blog/add-blog.component';
+import { ListUserComponent } from './components/auth/list-user/list-user.component';
+import { AddUserComponent } from './components/auth/add-user/add-user.component';
+import { ListCategoryComponent } from './components/category/list-category/list-category.component';
+import { AddCategoryComponent } from './components/category/add-category/add-category.component';
+import { ListProfileComponent } from './components/profile/list-profile/list-profile.component';
+import { AddProfileComponent } from './components/profile/add-profile/add-profile.component';
+import { ListWorkComponent } from './components/work/list-work/list-work.component';
+import { AddWorkComponent } from './components/work/add-work/add-work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlogComponent,
+    WorkComponent,
+    SignupComponent,
+    SigninComponent,
+    ListBlogComponent,
+    AddBlogComponent,
+    ListUserComponent,
+    AddUserComponent,
+    ListCategoryComponent,
+    AddCategoryComponent,
+    ListProfileComponent,
+    AddProfileComponent,
+    ListWorkComponent,
+    AddWorkComponent
   ],
   imports: [
     BrowserModule,

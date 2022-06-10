1abdc94e51750b8d2a73a14b96040974e9e9ea8e
daibede <xuandai300092@gmail.com>
2022-06-10 15:33:28 +0700

    server work

--- src/app/server/work.service.ts
+++ src/app/server/work.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Iwork } from '../model/work';
 @Injectable({
   providedIn: 'root'
 })
 export class WorkService {
+  URL_API= ` http://localhost:3000/works`
+  constructor(
+    private http : HttpClient
+  ) { }
 
-  constructor() { }
+  listWork(): Observable<Iwork[]>{
+    return this.http.get<Iwork[]>(`${this.URL_API}`)
+  }
+
+  detailWork(id: any): Observable<Iwork>{
+    return this.http.get<Iwork>(`${this.URL_API}/${id}`)
+  }
+
+  deleteWork(id: any): Observable<Iwork>{
+    return this.http.delete<Iwork>(`${this.URL_API}/${id}`)
+  }
+  addWork(work: Iwork): Observable<Iwork>{
+    return this.http.post<Iwork>(`${this.URL_API}`, work)
+  }
+
+  updateWork(work: Iwork): Observable<Iwork>{
+    return this.http.put<Iwork>(`${this.URL_API}/${work.id}`, work)
+  }
+
+
 }

32b0a8386d6b9d2695c77654ca7e25083b621f8d
daibede <xuandai300092@gmail.com>
2022-06-10 14:56:39 +0700

    router pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { WorkComponent } from './pages/work/work.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path:"", component: HomeComponent, pathMatch: "full"},
+   { path:"blog", component: BlogComponent},
+   { path:"work", component: WorkComponent},
+   { path:"signup", component: SignupComponent},
+   { path:"signin", component: SigninComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

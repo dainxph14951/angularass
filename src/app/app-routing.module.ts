7eed7c7de863095b8a235bdc7f877a49cc0bdeae
daibede <xuandai300092@gmail.com>
2022-06-10 15:03:17 +0700

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './components/auth/add-user/add-user.component';
+import { ListUserComponent } from './components/auth/list-user/list-user.component';
+import { AddBlogComponent } from './components/blog/add-blog/add-blog.component';
+import { ListBlogComponent } from './components/blog/list-blog/list-blog.component';
+import { AddCategoryComponent } from './components/category/add-category/add-category.component';
+import { ListCategoryComponent } from './components/category/list-category/list-category.component';
+import { AddProfileComponent } from './components/profile/add-profile/add-profile.component';
+import { ListProfileComponent } from './components/profile/list-profile/list-profile.component';
+import { AddWorkComponent } from './components/work/add-work/add-work.component';
+import { ListWorkComponent } from './components/work/list-work/list-work.component';
 import { HomeComponent } from './home/home.component';
 import { BlogComponent } from './pages/blog/blog.component';
 import { SigninComponent } from './pages/signin/signin.component';
@@ -13,7 +23,25 @@ const routes: Routes = [
    { path:"signup", component: SignupComponent},
    { path:"signin", component: SigninComponent},
 
+   { path:"admin/blog", component: ListBlogComponent},
+   { path:"admin/blog/add", component: AddBlogComponent},
+   { path:"admin/blog/:id/edit", component: AddBlogComponent},
 
+   { path:"admin/work", component: ListWorkComponent},
+   { path:"admin/work/add", component: AddWorkComponent},
+   { path:"admin/work/:id/edit", component: AddWorkComponent},
+
+   { path:"admin/category", component: ListCategoryComponent},
+   { path:"admin/category/add", component: AddCategoryComponent},
+   { path:"admin/category/:id/edit", component: AddCategoryComponent},
+
+   { path:"admin/auth", component: ListUserComponent},
+   { path:"admin/auth/add", component: AddUserComponent},
+   { path:"admin/auth/:id/edit", component: AddUserComponent},
+
+   { path:"admin/profile", component: ListProfileComponent},
+   { path:"admin/profile/add", component: AddProfileComponent},
+   { path:"admin/profile/:id/edit", component: AddProfileComponent},
 ];
 
 @NgModule({
